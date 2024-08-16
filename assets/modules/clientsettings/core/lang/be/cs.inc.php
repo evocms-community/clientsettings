291e3327543673ae181afe908cfafbc41afb3f89
Pathologic <m@xim.name>
2024-08-16 15:43:10 +0300

    refactor lexicons

---
+++ assets/modules/clientsettings/core/lang/be/cs.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+
+return [
+    'cs.module_title' => 'Налады сайту',
+    'cs.editor' => 'Рэдактар',
+    'cs.save' => 'Захаваць',
+    'cs.cancel' => 'Адмена',
+    'cs.close' => 'Закрыць',
+    'cs.stay' => 'Працягнуць рэдагаванне'
+];
