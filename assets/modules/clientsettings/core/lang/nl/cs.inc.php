0a8a238c943fa71428beac4bbcf674ebaf5cc8be
Pathologic <m@xim.name>
2024-08-26 16:15:52 +0300

    fix

--- assets/modules/clientsettings/core/lang/nl/cs.inc.php
+++ assets/modules/clientsettings/core/lang/nl/cs.inc.php
@@ -10,4 +10,7 @@ return [
     'dp_dayNames' => "['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag']",
     'dp_monthNames' => "['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']",
     'dp_startDay' => 1 ,
+    'saving' => 'Bezig met opslaan, een moment geduld a.u.b...',
+    'error_internet_connection' => 'Server is niet beschikbaar. Controleer je internetverbinding!',
+    'warning_not_saved' => 'U heeft de wijzigingen nog niet opgeslagen! U kunt op deze Pagina blijven om de wijzigingen op te slaan (\'Annuleren\'), of u kunt deze Pagina verlaten, waardoor eventuele wijzigingen verloren gaan (\'OK\').'
 ];
--- assets/modules/clientsettings/core/lang/pl/cs.inc.php
+++ assets/modules/clientsettings/core/lang/pl/cs.inc.php
@@ -10,4 +10,7 @@ return [
     'dp_dayNames' => "['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota']",
     'dp_monthNames' => "['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień']",
     'dp_startDay' => 1 ,
+    'saving' => 'Trwa zapisywanie, proszę czekać...',
+    'error_internet_connection' => 'Serwer jest niedostępny. Sprawdź swoje połączenie z internetem!',
+    'warning_not_saved' => 'Zmiany, których dokonałeś nie zostały jeszcze zapisane. Możesz pozostać na bieżącej stronie, aby zapisać zmiany (\'Anuluj\') lub opuścić ją, tracąc wszystkie dokonane przez Ciebie zmiany (\'OK\').'
 ];
